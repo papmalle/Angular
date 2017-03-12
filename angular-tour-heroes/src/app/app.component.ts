fbe137d1467ff66722bb2aafa66c5aa788af1cf1
yaya <papmalle@gmail.com>
2017-03-12 16:02:27 +0100

    Add myTagBranche

--- angular-tour-heroes/src/app/app.component.ts
+++ angular-tour-heroes/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
             
-             <p>List of Heroes : </p>
+             <p>List of Heroes myTagBranch: </p>
              <ul class="heroes">
                <li *ngFor="let hero of heroes"  [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
